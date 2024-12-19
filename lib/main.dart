19a9e16251f5195e761e09b71ed34f919fcb334a
boudyK <abdalrahmank2000@gmail.com>
2024-12-20 00:37:26 +0200

    Adding Notifications and testing

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:yarb/screens/splash_screen.dart';
 
+import 'notifications.dart';
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
@@ -12,10 +13,13 @@ final theme = ThemeData(
   textTheme: GoogleFonts.latoTextTheme(),
 );
 
-
-void main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // Initialize Notification Service
+  await NotificationService().initNotification();
+
   runApp(const MaterialApp(
     home: Scaffold(body: SplashScreen()),
   ));
